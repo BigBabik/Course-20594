8294342cfe60aee8e57deb21988cdc1dbcc0f957
BigBabik <yvbrn13@gmail.com>
2024-04-27 11:14:01 +0300

    added external func definition to syscall.c and added comma at the end of the syscall function array line

--- MMN11/syscall.c
+++ MMN11/syscall.c
@@ -103,6 +103,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_cps(void);
 
 static int (*syscalls[])(void) = {
 // Adding "new" "SYSCALLfork(fork)" without changing syscall.h
@@ -127,7 +128,7 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
-[22]	      sys_cps	// add the 22nd syscall without changing syscall.h, where the indexes are originally referrenced
+[22]	      sys_cps,	// add the 22nd syscall without changing syscall.h, where the indexes are originally referrenced
 };
 
 void
